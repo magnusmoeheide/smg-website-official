82e19d247ad4a8fad412f09bca1d332f8b8a5997
magnusmoeheide <magnus.heide@code.berlin>
2023-06-27 21:11:51 +0200

    Add allow refresh

--- src/App.js
+++ src/App.js
@@ -12,19 +12,19 @@ function App() {
   return (
     <div className="App">
       <Routes>
-            <Route exact path="/" element={<Home/>}/>
-            <Route path="/about" element={<About/>}/> 
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/> 
 
-            <Route path="/privacy" element={<Privacy/>}/> 
-            <Route path="/terms" element={<Terms/>}/> 
-            <Route path="/cookies" element={<Cookies/>}/> 
-            <Route path="/return" element={<Return/>}/> 
-            <Route path="/disclaimer" element={<Disclaimer/>}/> 
-            <Route path="/use" element={<AcceptableUse/>}/> 
-            <Route path="/eula" element={<Eula />}/> 
-            <Route path="/getstarted" element={<GetStarted />}/> 
-            <Route path="/articles/seatingarrangement" element={<SeatingArrangement />}/> 
-        </Routes>
+        <Route path="/privacy" element={<Privacy/>}/> 
+        <Route path="/terms" element={<Terms/>}/> 
+        <Route path="/cookies" element={<Cookies/>}/> 
+        <Route path="/return" element={<Return/>}/> 
+        <Route path="/disclaimer" element={<Disclaimer/>}/> 
+        <Route path="/use" element={<AcceptableUse/>}/> 
+        <Route path="/eula" element={<Eula />}/> 
+        <Route path="/getstarted" element={<GetStarted />}/> 
+        <Route path="/articles/seatingarrangement" element={<SeatingArrangement />}/> 
+      </Routes>
     </div>
   );
 }
