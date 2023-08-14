e04a8d6c0bca66255c27192c813b4e845010d6c9
magnusmoeheide <magnus.heide@code.berlin>
2023-08-14 16:45:04 +0200

    minor changes

--- src/container/Home/YoutubeEmbed.js
+++ src/container/Home/YoutubeEmbed.js
@@ -6,8 +6,7 @@ const YoutubeEmbed = ({ embedId }) => (
     <iframe
       width="853"
       height="480"
-      src={`https://www.youtube.com/embed/${embedId}`}
-      frameBorder="0"
+      src={`https://www.youtube-nocookie.com/embed/${embedId}`}
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
       title="Embedded youtube"
